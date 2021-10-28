0b7b9ba7db3b24d70e7f64c4214311f53e68f4ce
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-10-28 08:02:45 -0600

    APP Web

---
+++ AplicacionWeb/AplicacionWeb/Controllers/PersonaController.cs
@@ -0,0 +1,33 @@
+﻿using AplicacionWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionWeb.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult PersonaVista()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonaVista(Persona p)
+        {
+            _ = p;
+            return View();
+        }
+
+    }
+}
---
+++ bucleEjercicio/CapaEntidades/E_Maestros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaEntidades
+{
+    class E_Maestros
+    {
+        private int id;
+        private string nombres;
+        private string apellidos;
+        private int edad;
+        private string carrera;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombres { get => nombres; set => nombres = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Carrera { get => carrera; set => carrera = value; }
+
+        public E_Maestros() { }
+
+        public E_Maestros(int id, string nombres, string apellidos, int edad, string carrera)
+        {
+            this.id = id;
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.edad = edad;
+            this.carrera = carrera;
+        }
+    }
+}
